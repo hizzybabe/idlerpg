28f8a7c25e22b8e6c34837cc2f719b44b73337b6
hizzybabe <napipat_s@hotmail.com>
2024-10-29 15:20:36 +0700

    code optimization 3

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the public directory
+app.use(express.static('public'));
 
+// Handle the root route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
